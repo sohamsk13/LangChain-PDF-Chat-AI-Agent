35e995f72cf2e200645e4f7932c95613af7dd9ab
Soham Kulkarni <150679961+sohamsk13@users.noreply.github.com>
2025-09-27 08:31:17 +0530

    Delete chat.py

--- chat.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-from langchain.chat_models import init_chat_model
-
-load_dotenv()
-
-
-llm = init_chat_model("gemini-2.5-flash", temperature=0.3, api_key=os.getenv("GOOGLE_API_KEY"))
- 
